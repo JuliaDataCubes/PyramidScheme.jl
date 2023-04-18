ff7c88ca48de6ca5f441eea8db7f141731420da0
Felix Cremer <fcremer@bgc-jena.mpg.de>
2023-04-18 16:32:28 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.30

---
+++ src/PyramidScheme.jl
@@ -0,0 +1,5 @@
+module PyramidScheme
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PyramidScheme
+using Test
+
+@testset "PyramidScheme.jl" begin
+    # Write your tests here.
+end
