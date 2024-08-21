61c241c0fb6e553a81000c26044a43ceaac4398b
Felix Cremer <fcremer@bgc-jena.mpg.de>
2024-08-21 10:34:16 +0200

    Rearrange scatterplot

--- docs/src/scatter_example.jl
+++ docs/src/scatter_example.jl
@@ -19,18 +19,18 @@ rasprecipscale = rasprecip.metadata["scale"] .* rasprecip .+ rasprecip.metadata[
 
 pyrprecip = Pyramid(rasprecip)
 
-scattas = Observable(vec(collect(pyrtas.levels[end])))
-scatprecip = Observable(vec(collect(pyrprecip.levels[end])))
+#scattas = Observable(vec(collect(pyrtas.levels[end])))
+#scatprecip = Observable(vec(collect(pyrprecip.levels[end])))
 
 scatpoints = Observable(Point2f[])
 
-begin
-figmap = Figure()
-axtas = Axis(figmap[1,1])
+figmap = begin
+    figmap = Figure()
+axtas = Axis(figmap[1,1], aspect=DataAspect())
 plot!(axtas, pyrtas, colormap=:berlin);
-axprecip = Axis(figmap[1,2])
-plot!(axprecip, pyrprecip)
-axscat = Axis(figmap[2,1:2])
+axprecip = Axis(figmap[2,1], aspect=DataAspect())
+plot!(axprecip, pyrprecip, colormap=Reverse(:broc))
+axscat = Axis(figmap[1:2, 2])
 #scatter!(axscat, scatpoints, scatprecip)
 #datashader!(axscat, scatpoints)
 on(axtas.finallimits) do limits
@@ -40,12 +40,15 @@ on(axtas.finallimits) do limits
     if Extents.intersects(rasdataext, datalimit)
         scatpoints.val = Point2f.(vec(collect(PyramidScheme.selectlevel(pyrtas, datalimit, target_imsize=axtas.scene.viewport[].widths))),
                             vec(collect(PyramidScheme.selectlevel(pyrprecip, datalimit, target_imsize=axtas.scene.viewport[].widths))))
-
+        xext = extrema(first.(scatpoints.val))
+        yext = extrema(last.(scatpoints.val))
+        limits!(axscat, xext, yext)
     end
     notify(scatpoints)
 #    notify(scatprecip)
 end
 datashader!(axscat, scatpoints)
 linkaxes!(axtas, axprecip)
+hidexdecorations!(axtas)
 figmap
 end
