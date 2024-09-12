fc727fc5204d1275514f23f5efee3389033d84bc
Felix Cremer <fcremer@bgc-jena.mpg.de>
2024-09-12 13:59:08 +0200

    Wrap tests in testitems and add broadcast test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,9 @@
-using PyramidScheme:PyramidScheme as PS
-using Test
-using DimensionalData
+using Test, TestItemRunner
 
-@testset "Pyramid" begin
+@run_package_tests@testset "Pyramid" begin
     using DimensionalData
     using PyramidScheme: PyramidScheme as PS
+    using Makie
     data = rand(2000,2000)
     dd = DimArray(data, (X(1:2000), Y(1:2000)))
     pyramid = PS.Pyramid(dd)
@@ -15,8 +14,22 @@ using DimensionalData
     @test parent(subpyramid) == data[1:10,1:10]
     fig, axis, h = plot(pyramid)
 end
-@testset "Helper functions" begin
+@testitem "Helper functions" begin
+    using PyramidScheme: PyramidScheme as PS
     @test PS.compute_nlevels(zeros(1000)) == 0
     @test PS.compute_nlevels(zeros(1000,1025)) == 1
     @test PS.compute_nlevels(zeros(10000, 8000)) == 4
+end
+
+@testitem "Broadcast" begin
+    using DimensionalData
+    using PyramidScheme: PyramidScheme as PS
+    data1 = rand(2000,2000)
+    dd1 = DimArray(data1, (X(1:2000), Y(1:2000)))
+    pyramid1 = PS.Pyramid(dd1)
+    data2 = rand(2000,2000)
+    dd2 = DimArray(data2, (X(1:2000), Y(1:2000)))
+    pyramid2 = PS.Pyramid(dd2)
+    pyrsum = pyramid1 .+ pyramid2
+    @test pyrsum.levels[end][1,1] == pyramid1.levels[end][1,1] + pyramid2.levels[end][1,1]
 end
