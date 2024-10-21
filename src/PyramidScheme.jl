83cb720a2cef30ac990cc82e1a1acd39f4c69703
Felix Cremer <fcremer@bgc-jena.mpg.de>
2024-10-21 16:45:13 +0200

    Fix loading a Pyramid in 1.11

--- src/PyramidScheme.jl
+++ src/PyramidScheme.jl
@@ -64,7 +64,7 @@ function Pyramid(path::AbstractString, backend)
 end
 function _pyramid_gdal end
 
-function _pyramid_zarr(path)
+function _load_pyramid(path, backend)
     g = zopen(path)
     allkeys = collect(keys(g.groups))
     base = Cube(path) # This getindex should be unnecessary and I should rather fix my data on disk
