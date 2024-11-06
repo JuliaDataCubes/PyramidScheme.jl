31a4fdb4843f3780da218cf419764eb2322b1a18
Felix Cremer <fcremer@bgc-jena.mpg.de>
2024-11-06 14:00:07 +0100

    replace zero with nan and cache data

--- test/tilepyramids.jl
+++ test/tilepyramids.jl
@@ -1,25 +1,27 @@
 import PyramidScheme as PS
-using Tyler: Map, wgs84
+#using Tyler: Map, wgs84
 using Extents: Extent
-import GLMakie
+#import GLMakie
 using HTTP
+using DimensionalData.Dimensions
 @dim lon XDim "Longitude"
 
 @dim lat YDim "Latitude"
-p2020 = PS.Pyramid("https://s3.bgc-jena.mpg.de:9000/pyramids/ESACCI-BIOMASS-L4-AGB-MERGED-100m-2020-fv4.0.zarr")
-p2017 = PS.Pyramid("https://s3.bgc-jena.mpg.de:9000/pyramids/ESACCI-BIOMASS-L4-AGB-MERGED-100m-2017-fv4.0.zarr")
+replacenan(data) =  data <= 0 ? NaN32 : Float32(data)
+p2020 = replacenan.(PS.Pyramid("https://s3.bgc-jena.mpg.de:9000/pyramids/ESACCI-BIOMASS-L4-AGB-MERGED-100m-2020-fv4.0.zarr"))
+p2017 = replacenan.(PS.Pyramid("https://s3.bgc-jena.mpg.de:9000/pyramids/ESACCI-BIOMASS-L4-AGB-MERGED-100m-2017-fv4.0.zarr"))
 agbdiff = p2017 .- p2020
-datarange = (0.0,400.0)
+datarange = (1.0,200.0)
 
-pv = PS.PyramidProvider(p2020,datarange...,colorscheme=:speed)
+pv = PS.PyramidProvider(PS.cache(p2020),datarange...,colorscheme=:speed)
 
 ext = Extent(X=(-180.0,180.0),Y=(-60.0,80.0))
 
 
-s = HTTP.serve!(pv,"127.0.0.1",8765)
+s = HTTP.serve!(pv,"127.0.0.1",8766)
 
 
-datarange = (-20. , 20.)
-diffprov = PS.PyramidProvider(agbdiff, datarange..., colorscheme=:diverging_gkr_60_10_c40_n256)
-s = HTTP.serve!(diffprov,"127.0.0.1",8766)
+datarange = (-15. , 15.)
+diffprov = PS.PyramidProvider(PS.cache(agbdiff), datarange..., colorscheme=:diverging_gkr_60_10_c40_n256)
+sd = HTTP.serve!(diffprov,"127.0.0.1",8767)
 prov = TileProviders.Provider("http://127.0.0.1:8765/{z}/{x}/{y}.png")
