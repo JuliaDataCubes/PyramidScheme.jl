d5a960145931f5b7adf753bc07e2ef23a8ca4c2e
Felix Cremer <fcremer@bgc-jena.mpg.de>
2025-01-10 03:59:21 -0100

    Add broadcast test

--- test/broadcast.jl
+++ test/broadcast.jl
@@ -3,6 +3,15 @@
     using DimensionalData
     using ArchGDAL
 
-    
+    pyr = Pyramid("test/data/pyramidmiddle.tif")
+    p0 = pyr .- pyr
+    @test all(iszero, p0.base)
+    @test all(all.(iszero, p0.levels))
+
+    p1 = p0 .+ 1
+    @test sum(p1.base) == prod(size(p1.base))
+    for l in p1.levels
+        @test sum(l) == prod(size(l))
+    end
     
 end
