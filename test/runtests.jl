be4c9847da0c293ddf0cd4172f5e32fdd89c04f3
Felix Cremer <fcremer@bgc-jena.mpg.de>
2026-02-03 13:18:25 +0100

    Add isequal dispatch on Pyramid (#91)
    * Add isequal dispatch on Pyramid
    
    This tests isequal on all levels separately this should be faster and also more correct. This will also catch if two pyramids are from the same data but with different aggregation functions.
    
    * Add test for different base but same levels and for different number of levels
    
    * Make Makie imports explicit

--- test/runtests.jl
+++ test/runtests.jl
@@ -168,6 +168,7 @@ end
     using DimensionalData
     pyr1 = Pyramid(fill(1,X(1:128),Y(1:128)), tilesize=16)
     pyr2 = Pyramid(fill(1, X(1:128),Y(1:128)), tilesize=16, resampling_method=sum)
+
     pyr1_neg = map(x-> x-1, pyr1)
     @test all(all.(iszero, pyr1_neg.levels))
     @test iszero(pyr1_neg.base)
@@ -195,6 +196,27 @@ end
     @test cb.limits[] == [0.0, 2.0]
     limits!(ax, 200,300, 200, 300)
     @test cb.limits[] == [-0.5, 0.5]
+end
+
+@testitem "isequal of two pyramids" begin
+    using DimensionalData
+    pyr1 = Pyramid(fill(1, X(1:128),Y(1:128)), tilesize=16)
+    pyr2 = Pyramid(fill(1, X(1:128),Y(1:128)), tilesize=16, resampling_method=sum)
+    @test !isequal(pyr1, pyr2)
+
+    a = fill(1, 100,100)
+    a[1:2:end, 1:2:end] .= 2
+    a[2:2:end, 2:2:end] .= 2
+    dda = DimArray(a, (X(1:100), Y(1:100)))
+    pyra = Pyramid(dda, tilesize=25)
+    
+    b = fill(2, 100,100)
+    b[1:2:end, 1:2:end] .= 1
+    b[2:2:end, 2:2:end] .= 1
+    ddb = DimArray(b, (X(1:100), Y(1:100)))
+    pyrb = Pyramid(ddb, tilesize=25)
+    @test !isequal(pyra, pyrb)
+    @test !isequal(pyra, pyr1)
 end
 #=
 @testitem "Comparing zarr pyramid with tif pyramid" begin
